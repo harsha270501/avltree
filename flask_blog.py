e0d188ad78c595627f632c56fe6db9398fa2a30a
vsvt2000 <viswajithvishnusai@gmail.com>
2021-04-06 22:05:07 +0530

    Brought ankis changes to main.Plz check all if its working fine

--- flask_blog.py
+++ flask_blog.py
@@ -12,5 +12,9 @@ formpref=''
 @app.route('/home')
 def home():
     return render_template('home.html')
+@app.route('/visualization')
+def visualization():
+    return render_template('visualize.html')
 if __name__ == "__main__":
     app.run(debug=True)
+
